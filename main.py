1e8fe6b4b2cd071b4ea4d610097d650fc0e53bd5
Abubakir-ux <abubakirtohirov720@gmail.com>
2026-03-10 09:44:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,14 @@ def run_bot():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--proxy-server='direct://'")
+    chrome_options.add_argument("--proxy-bypass-list=*")
     chrome_options.add_argument("--window-size=1920,1080")
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    wait = WebDriverWait(driver, 60)
+    wait = WebDriverWait(driver, 120)
 
     try:
         print("Banklar tekshirilmoqda...")
