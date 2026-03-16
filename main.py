50d6043ade41cda00f74401f8c247af6de063d6b
Abubakir-ux <abubakirtohirov720@gmail.com>
2026-03-16 10:00:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.set_page_load_timeout(180)
-wait = WebDriverWait(driver, 45)
+wait = WebDriverWait(driver, 60)
 
 try:
     print("Ma'lumotlar yig'ilmoqda...")
